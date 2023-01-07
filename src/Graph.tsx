954700bd23b25821e9f55a95b0e85d95c35ed0fb
sevde aksoy <aksoyeminesevde@gmail.com>
2023-01-07 21:08:54 +0200

    commit

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -32,8 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: document.getElementsByTagName ('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
-
+    const elem = document.getElementsByTagName ('perspective-viewer')[0] as PerspectiveViewerElement;
     const schema = {
       stock: 'string',
       top_ask_price: 'float',
